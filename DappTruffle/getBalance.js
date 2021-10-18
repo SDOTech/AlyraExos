356029e6aebaaa177c4ea51e88d01eb76e41f7df
xstahef <stephane@dorlac.com>
2021-10-18 11:27:26 +0200

    Correctif

--- DappTruffle/getBalance.js
+++ DappTruffle/getBalance.js
@@ -1,8 +1,27 @@
-const Web3 = require('web3')
-const rpcURL = "https://mainnet.infura.io/v3/0xd804ab1667e940052614a5acd103dde4d298ce36"
-const web3 = new Web3(rpcURL)
-
-web3.eth.getBalance("0xb683d83a532e2cb7dfa5275eed3698436371cc9f", (err, wei) => { 
-   balance = web3.utils.fromWei(wei, 'ether'); // convertir la valeur en ether
-   console.log(balance);
+var  Web3  =  require('web3');
+web3  =  new  Web3(new  Web3.providers.HttpProvider('https://mainnet.infura.io/v3/461e417d912441f78958e476ebc11f47'));
+    
+console.log('Getting contract tokens balance.....');
+    
+var  addr  = ('0xd804ab1667e940052614a5acd103dde4d298ce36');
+    
+console.log("Address: "  +  addr);
+    
+var  contractAddr  = ('0xb683d83a532e2cb7dfa5275eed3698436371cc9f');
+var  tknAddress  = (addr).substring(2);
+    
+// '0x70a08231' est le hexa de la fonction "balanceOf" de l'ERC20
+var  contractData  = ('0x70a08231000000000000000000000000'  +  tknAddress);
+    
+web3.eth.call({
+	to: contractAddr,
+	data: contractData 
+}, function(err, result) {
+	if (result) {
+		var  tokens  =  web3.utils.toBN(result).toString(); 
+		    console.log('Tokens Owned: '  +  web3.utils.fromWei(tokens, 'ether')); 
+	}
+	else {
+		console.log(err); // Dump errors here
+	}
 });
