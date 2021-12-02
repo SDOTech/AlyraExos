c48cbc9cca6d2336a2a58d13048196b3c6b1c126
SDO <stephane.dorlac@protonmail.com>
2021-12-02 12:14:10 +0100

    add tests

--- DEFI_03/test/voting.test.js
+++ DEFI_03/test/voting.test.js
@@ -1,25 +1,34 @@
 const Voting = artifacts.require("./Voting.sol");
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const truffleAssert = require('truffle-assertions');
 
 contract("Voting", accounts => {
 
+  // Should Register accounts[1] From account[0]
   it("...should Register accounts[1] From account[0]", async () => {    
     const VotingInstance = await Voting.deployed();
-
-    // Register accounts[1] From account[0]
+    
     await VotingInstance.registeringUsers(accounts[1], { from: accounts[0] });
     const registeredUser = await VotingInstance.getVoter(accounts[1], { from: accounts[0] });
     assert.equal(registeredUser.isRegistered, true, "accounts[1] was not registered.");    
   });
 
+  // Should NOT register twice accounts[1] from account[0]
   it("...should NOT register twice accounts[1] from account[0]", async () => {
-    const VotingInstance = await Voting.deployed();
-
-  // try to register account[1] twice
+    const VotingInstance = await Voting.deployed();  
     await truffleAssert.reverts(VotingInstance.registeringUsers(accounts[1], { from: accounts[0] }), "Existing Address !");
   });
 
+  // check workflow status after admin open Proposal Registration
+  // workflowstatus shoulb be ProposalsRegistrationStarted
+  it("...workflowstatus should be ProposalsRegistrationStarted", async () => {
+    const VotingInstance = await Voting.deployed();  
+
+    //admin openProposaRegistration
+    const trx = VotingInstance.openProposaRegistration({ from: accounts[0] });    
+    assert.equal(VotingInstance._workflowStatus.valueOf(),1, "WorkflowStatus not correct");    
+
+  });
 
 
  
