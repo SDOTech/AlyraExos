b8becd275b56fe56b2775f579bc36041fd4ced54
SDO <stephane.dorlac@protonmail.com>
2021-12-06 13:59:22 +0100

    add deploy changes

--- DEFI_03/truffle-config.js
+++ DEFI_03/truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config()  // Store environment-specific variable from '.env' to process.env
 const path = require("path");
 
 module.exports = {
@@ -9,7 +11,15 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // 7545 = Ganache Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
-     },
+    },
+    
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),      
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+
   },
    // Configure your compilers
    compilers: {
